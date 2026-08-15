f0df2e83dfc69530dbe72edac5cf1bb3046caa46
Rahull Ganesh M <rahull.ganesh@idp.com>
2026-08-15 20:17:36 +0530

    Deployed Backend

--- test-portal-hire360/frontend/src/services/api.js
+++ test-portal-hire360/frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Base URL of FastAPI backend
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  //"https://ylmuevgvjd.execute-api.ap-southeast-1.amazonaws.com",
+  baseURL: "https://ylmuevgvjd.execute-api.ap-southeast-1.amazonaws.com",
+  //"",
   headers: {
     "Content-Type": "application/json",
   },
